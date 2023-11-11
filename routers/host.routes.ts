922fb93eb1d807965bb93523c63b3b58641d8491
Muhammad Shaker Kasem AlKilany <m.sh64944@gmail.com>
2023-11-11 23:16:42 +0200

    host for admi

--- routers/host.routes.ts
+++ routers/host.routes.ts
@@ -2,6 +2,10 @@ import { Router } from "express";
 import {
   addAdmin,
   addHost,
+  deleteHost,
+  edit,
+  editHost,
+  getAll,
   getSpecificHost,
   getUserHost,
   removAdmin,
@@ -11,6 +15,7 @@ import {
   joiValidatorParams,
 } from "../middleware/joiValidator";
 import { hostAdminSchema, hostSchema } from "../joi/host.joi";
+import { guardAdmin } from "../middleware/Guard/guardAdmin";
 const router = Router();
 router.post("/", joiValidatorBody(hostSchema), addHost);
 router.get("/all_user_host", getUserHost);
@@ -25,4 +30,6 @@ router.patch(
   joiValidatorParams(hostAdminSchema),
   removAdmin
 );
+router.get("/all",guardAdmin,getAll)
+router.route("/:id").all(guardAdmin).delete(deleteHost).patch(edit)
 export default router;
