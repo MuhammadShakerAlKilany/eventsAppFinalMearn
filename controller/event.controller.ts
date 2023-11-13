dc16712de266f35ad77820eb926c5d5694d81b4b
Mohamed Nasr <mhmdnsr.dev@gmail.com>
2023-11-13 10:38:42 +0200

    Make "delete (host/event/place)" appropriate to multiple scenarios

--- controller/event.controller.ts
+++ controller/event.controller.ts
@@ -2,168 +2,210 @@ import { ObjectId, Schema } from "mongoose";
 import EventDao from "../dao/event.dao";
 import { EventApp, EventCreat } from "../interfaces/event.interface";
 import tryCatchErr from "../middleware/tryCatchErr";
-import { EventEmitter } from "events"
-import { scheduleJob } from "node-schedule"
+import { EventEmitter } from "events";
+import { scheduleJob } from "node-schedule";
 import UserDao from "../dao/user.dao";
 import HostDao from "../dao/host.dao";
 import PlaceDao from "../dao/place.dao";
 import { Response } from "express-serve-static-core";
 import { date } from "joi";
-import path from "path"
-import { getDownloadURL, getStorage, ref, uploadBytesResumable } from "firebase/storage";
+import path from "path";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
 import { initializeApp } from "firebase/app";
 import { stor } from "../modules/firebasestorage";
-export const newEventNoti = new EventEmitter()
-const eventDao = new EventDao()
-const userDao = new UserDao()
-const hostDao = new HostDao()
-const placeDao = new PlaceDao()
-
+import AdminDao from "../dao/admin.dao";
+export const newEventNoti = new EventEmitter();
+const eventDao = new EventDao();
+const userDao = new UserDao();
+const hostDao = new HostDao();
+const placeDao = new PlaceDao();
 
 export const eventCreat = tryCatchErr<EventCreat>(async (req, res) => {
-    const event = req.body
-    const userId = req["user"]._id
-    const userFind = await userDao.findById(userId);
-    if(!userFind)return res.status(404).json({message:"you logout"})
-    const eventNum = userFind?.subscribeWith.length
-if(eventNum>=3 && !userFind.isVIP)return res.status(400).json({message:"chang your plan to add more event"})
-const host = await hostDao.findHost(event?.host);
-if (!host) return res.status(404).json({ message: "not found host" })
+  const event = req.body;
+  const userId = req["user"]._id;
+  const userFind = await userDao.findById(userId);
+  if (!userFind) return res.status(404).json({ message: "you logout" });
+  const eventNum = userFind?.subscribeWith.length;
+  if (eventNum >= 3 && !userFind.isVIP)
+    return res
+      .status(400)
+      .json({ message: "chang your plan to add more event" });
+  const host = await hostDao.findHost(event?.host);
+  if (!host) return res.status(404).json({ message: "not found host" });
 
-if (event.place) {
+  if (event.place) {
     const place = await placeDao.findById(event?.place);
-    if (!place) return res.status(404).json({ message: "not found place" })
-}
-if(req.file){
-    const filename = ""+req.file.size+req.file.originalname+new Date()
-    const storRef = ref(stor, filename)
+    if (!place) return res.status(404).json({ message: "not found place" });
+  }
+  if (req.file) {
+    const filename = "" + req.file.size + req.file.originalname + new Date();
+    const storRef = ref(stor, filename);
     const snapSot = await uploadBytesResumable(storRef, req.file.buffer, {
-            contentType: req.file.mimetype
-        })
-     const dowURL = await getDownloadURL(snapSot.ref)
-    event.posterPath = dowURL
-}
-    const newEvent = await eventDao.createEvent(event)
-    newEventNoti.emit("new_event", newEvent)
-    // const date = new Date(newEvent.dateTime)
-    // date.setHours(date.getHours() - 1)
-    // scheduleJob(date, async () => {
-    //     const event = await eventDao.findEvent(newEvent._id)
-    //     if (event) {
-    //         event.subscribers.forEach((userId) => {
-    //             eventEmitter.emit(`event-${userId}`, event.title)
-    //         })
-    //     }
-    // })
-    res.status(201).json({ message: "event created", data: newEvent })
-})
+      contentType: req.file.mimetype,
+    });
+    const dowURL = await getDownloadURL(snapSot.ref);
+    event.posterPath = dowURL;
+  }
+  event.createdBy = userId;
+  const newEvent = await eventDao.createEvent(event);
+  newEventNoti.emit("new_event", newEvent);
+  // const date = new Date(newEvent.dateTime)
+  // date.setHours(date.getHours() - 1)
+  // scheduleJob(date, async () => {
+  //     const event = await eventDao.findEvent(newEvent._id)
+  //     if (event) {
+  //         event.subscribers.forEach((userId) => {
+  //             eventEmitter.emit(`event-${userId}`, event.title)
+  //         })
+  //     }
+  // })
+  res.status(201).json({ message: "event created", data: newEvent });
+});
 export const allEvent = tryCatchErr(async (req, res) => {
-    const events = await eventDao.getAllEvent()
-    return res.json({ message: "all events", data: events })
-})
+  const events = await eventDao.getAllEvent();
+  return res.json({ message: "all events", data: events });
+});
 export const allEventUser = tryCatchErr(async (req, res) => {
-    const events = await eventDao.getAllEvent()
-    return res.json({ message: "all events", data: events })
-})
-export const findEvent = tryCatchErr<never, { _id: ObjectId }>(async (req, res) => {
-    const _id = req.params._id
-    const event = await eventDao.findEventWithUser(_id)
-    if (!event) return res.json({ message: "not find events", data: { _id } })
-    return res.json({ message: "find events", data: event })
-})
-export const findEventForUser = tryCatchErr<never, { _id: ObjectId }>(async (req, res) => {
-    const _id = req.params._id
-    const userId = req["user"]._id;
-    if (!(await isAdmin(_id, userId, res))) return
-    const event = await eventDao.findEventWithUser(_id)
-    if (!event) return res.json({ message: "not find event", data: { _id } })
-    return res.json({ message: "find event", data: event })
-})
-export const subscribe = tryCatchErr<never, { _id: ObjectId }>(async (req, res) => {
-    const eventId = req.params._id;
-    console.log(eventId)
-    const userId = req["user"]._id;
-    const user = await userDao.findById(userId)
-    if (!user) return res.status(404).json({ message: "not found user", data: { userId } })
-    const event = await eventDao.eventSubscribe(eventId, userId)
-    if (!event) return res.status(404).json({ message: "not found event", data: { eventId } })
-    return res.json({ message: "subscribe success" })
-})
-export const unsubscribe = tryCatchErr<never, { _id: ObjectId }>(async (req, res) => {
-    const eventId = req.params._id;
-    const userId = req["user"]._id;
-    const user = await userDao.findById(userId)
-    if (!user) return res.status(404).json({ message: "not found user", data: { userId } })
-    const event = await eventDao.eventUnSubscribe(eventId, userId)
-    if (!event) return res.status(404).json({ message: "not found event", data: { eventId } })
-    return res.json({ message: "unSubscribe success" })
-})
-export const userEventAmin = tryCatchErr(async (req, res) => {
-    const userId = req["user"]._id;
-    const events = await eventDao.userEventAmin(userId)
-    return res.json({ message: "all events", data: events })
-})
-export const edit = tryCatchErr<EventApp, { _id: ObjectId }>(async (req, res) => {
-    const userId = req["user"]._id;
-    const eventId = req.params._id;
-    if (!(await isAdmin(eventId, userId, res))) return
-    const eventData = req.body;
-    if(req.file){
-        const filename = ""+req.file.size+req.file.originalname+new Date()
-        const storRef = ref(stor, filename)
-        const snapSot = await uploadBytesResumable(storRef, req.file.buffer, {
-                contentType: req.file.mimetype
-            })
-         const dowURL = await getDownloadURL(snapSot.ref)
-         eventData.posterPath = dowURL
-    }
-    const event = await eventDao.edit(eventId, eventData)
-    return res.json({ message: "even edit", data: event })
-})
-export const getEventSubscribeWith = tryCatchErr(async (req, res) => {
-    const userId = req["user"]._id;
-    const events = await eventDao.getEventSubscribeWith(userId);
-    return res.json({ message: "event subscribe with", data: events })
-})
-export const deleteEvent = tryCatchErr<never, { _id: ObjectId }>(async (req, res) => {
+  const events = await eventDao.getAllEvent();
+  return res.json({ message: "all events", data: events });
+});
+export const findEvent = tryCatchErr<never, { _id: ObjectId }>(
+  async (req, res) => {
     const _id = req.params._id;
-    const event = await eventDao.delete(_id)
-    if(!event)return res.status(404).json({message:"not found event"});
-    return res.json({ message: "delete event", data: event })
-})
-export const eventPhoto =tryCatchErr<never,{ _id: ObjectId }>(async (req, res) => {
-    const _id = req.params._id
-  const event = await eventDao.findEvent(_id);
-  if(!event)return res.status(404).json({message:"not found event"});
- return res.sendFile(path.join(__dirname,"..",event.posterPath));
-})
+    const event = await eventDao.findEventWithUser(_id);
+    if (!event) return res.json({ message: "not find events", data: { _id } });
+    return res.json({ message: "find events", data: event });
+  }
+);
+export const findEventForUser = tryCatchErr<never, { _id: ObjectId }>(
+  async (req, res) => {
+    const _id = req.params._id;
+    const userId = req["user"]._id;
+    if (!(await isAdmin(_id, userId, res))) return;
+    const event = await eventDao.findEventWithUser(_id);
+    if (!event) return res.json({ message: "not find event", data: { _id } });
+    return res.json({ message: "find event", data: event });
+  }
+);
+export const subscribe = tryCatchErr<never, { _id: ObjectId }>(
+  async (req, res) => {
+    const eventId = req.params._id;
+    console.log(eventId);
+    const userId = req["user"]._id;
+    const user = await userDao.findById(userId);
+    if (!user)
+      return res
+        .status(404)
+        .json({ message: "not found user", data: { userId } });
+    const event = await eventDao.eventSubscribe(eventId, userId);
+    if (!event)
+      return res
+        .status(404)
+        .json({ message: "not found event", data: { eventId } });
+    return res.json({ message: "subscribe success" });
+  }
+);
+export const unsubscribe = tryCatchErr<never, { _id: ObjectId }>(
+  async (req, res) => {
+    const eventId = req.params._id;
+    const userId = req["user"]._id;
+    const user = await userDao.findById(userId);
+    if (!user)
+      return res
+        .status(404)
+        .json({ message: "not found user", data: { userId } });
+    const event = await eventDao.eventUnSubscribe(eventId, userId);
+    if (!event)
+      return res
+        .status(404)
+        .json({ message: "not found event", data: { eventId } });
+    return res.json({ message: "unSubscribe success" });
+  }
+);
+export const userEventAmin = tryCatchErr(async (req, res) => {
+  const userId = req["user"]._id;
+  const events = await eventDao.userEventAmin(userId);
+  return res.json({ message: "all events", data: events });
+});
+export const edit = tryCatchErr<EventApp, { _id: ObjectId }>(
+  async (req, res) => {
+    const userId = req["user"]._id;
+    const eventId = req.params._id;
+    if (!(await isAdmin(eventId, userId, res))) return;
+    const eventData = req.body;
+    if (req.file) {
+      const filename = "" + req.file.size + req.file.originalname + new Date();
+      const storRef = ref(stor, filename);
+      const snapSot = await uploadBytesResumable(storRef, req.file.buffer, {
+        contentType: req.file.mimetype,
+      });
+      const dowURL = await getDownloadURL(snapSot.ref);
+      eventData.posterPath = dowURL;
+    }
+    const event = await eventDao.edit(eventId, eventData);
+    return res.json({ message: "even edit", data: event });
+  }
+);
+export const getEventSubscribeWith = tryCatchErr(async (req, res) => {
+  const userId = req["user"]._id;
+  const events = await eventDao.getEventSubscribeWith(userId);
+  return res.json({ message: "event subscribe with", data: events });
+});
+export const deleteEvent = tryCatchErr<never, { _id: ObjectId }>(
+  async (req, res) => {
+    const _id = req.params._id;
+    const userId = req["user"]._id;
+    const event = await eventDao.findEvent(_id);
+    if (!event) return res.status(404).json({ message: "not found event" });
+    const admin = await new AdminDao().getAdmin(userId);
+    if (!(admin || event.createdBy.toString() === userId))
+      return res.status(403).json({ message: "You can not delete this event" });
+    await eventDao.delete(_id);
+    return res.status(200).json({ message: "delete event", data: event });
+  }
+);
+export const eventPhoto = tryCatchErr<never, { _id: ObjectId }>(
+  async (req, res) => {
+    const _id = req.params._id;
+    const event = await eventDao.findEvent(_id);
+    if (!event) return res.status(404).json({ message: "not found event" });
+    return res.sendFile(path.join(__dirname, "..", event.posterPath));
+  }
+);
 
 async function isAdmin(eventId: ObjectId, userId: ObjectId, res: Response) {
-    const eventFind = await eventDao.findEvent(eventId)
-    if (!eventFind) {
-        res.status(404).json({ message: "not found event" });
-        return false 
-    }
-    const isAdmin = (await hostDao.findHost(eventFind.host))?.admins.includes(userId);
-    
-    if (!isAdmin){
-        res.status(403).json({ message: "you not admin for event" });
-        return false
-    } 
-    return true
+  const eventFind = await eventDao.findEvent(eventId);
+  if (!eventFind) {
+    res.status(404).json({ message: "not found event" });
+    return false;
+  }
+  const isAdmin = (await hostDao.findHost(eventFind.host))?.admins.includes(
+    userId
+  );
+
+  if (!isAdmin) {
+    res.status(403).json({ message: "you not admin for event" });
+    return false;
+  }
+  return true;
 }
 export const editWithAdmin = tryCatchErr<EventApp, { _id: ObjectId }>(
   async (req, res) => {
     const eventId = req.params._id;
     const eventData = req.body;
-    if(req.file){
-        const filename = ""+req.file.size+req.file.originalname+new Date()
-        const storRef = ref(stor, filename)
-        const snapSot = await uploadBytesResumable(storRef, req.file.buffer, {
-                contentType: req.file.mimetype
-            })
-         const dowURL = await getDownloadURL(snapSot.ref)
-         eventData.posterPath = dowURL
+    if (req.file) {
+      const filename = "" + req.file.size + req.file.originalname + new Date();
+      const storRef = ref(stor, filename);
+      const snapSot = await uploadBytesResumable(storRef, req.file.buffer, {
+        contentType: req.file.mimetype,
+      });
+      const dowURL = await getDownloadURL(snapSot.ref);
+      eventData.posterPath = dowURL;
     }
     const event = await eventDao.edit(eventId, eventData);
     return res.json({ message: "event edited", data: event });
--- interfaces/event.interface.ts
+++ interfaces/event.interface.ts
@@ -1,16 +1,17 @@
 import { ObjectId } from "mongoose";
 
-export interface EventApp{
-    _id:ObjectId
-    category:string,
-    title:string,
-    dateTime:Date,
-    description:string,
-    subscribers:ObjectId[],
-    host:ObjectId,
-    place:ObjectId,
-    ticketCount:number,
-    posterPath: string,
-    location: string,
+export interface EventApp {
+  _id: ObjectId;
+  category: string;
+  title: string;
+  dateTime: Date;
+  description: string;
+  subscribers: ObjectId[];
+  host: ObjectId;
+  place: ObjectId;
+  ticketCount: number;
+  posterPath: string;
+  location: string;
+  createdBy: ObjectId;
 }
-export type EventCreat=Omit<EventApp,"subscribers"|"_id">
+export type EventCreat = Omit<EventApp, "subscribers" | "_id">;
